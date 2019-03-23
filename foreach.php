71bad0b77ab7a63f2f722e44c7092fdc4518b7e4
sahininayanthara <sahininayanthara95@gmail.com>
2019-03-23 08:57:20 +0530

    first commit

---
+++ foreach.php
@@ -0,0 +1,21 @@
+<html>
+<body>
+
+<?php
+
+
+$array= array("Sanduni", "Kasun", "Amal", "Nisha", "Sanka");
+
+foreach($array as $loopdata){
+echo "My name is " . $loopdata . ".<br>";
+
+
+
+}
+
+
+
+?>
+
+</body>
+</html>
